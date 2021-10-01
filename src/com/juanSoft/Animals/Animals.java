d908fb166ee2c66fb363fbb5c6cc066ec94d833e
Juan Ochando <usr2668@salesianos-lacuesta.net>
2021-10-01 16:54:06 +0100

    feature: se creo el proyecto

---
+++ src/com/juanSoft/Animals/Animals.java
@@ -0,0 +1,17 @@
+package com.juanSoft.Animals;
+
+public class Animals {
+
+    private String name;
+    private int age;
+    private String gender;
+
+    public Animals(String name, int age,String gender) {
+       this.name = name;
+       this.age = age;
+       this.gender = gender;
+
+    }
+
+
+}
