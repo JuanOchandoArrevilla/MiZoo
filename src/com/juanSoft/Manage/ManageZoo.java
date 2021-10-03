ba6884d61b044ef97e1fc326840a5b7f3f7570f6
Juan Ochando <usr2668@salesianos-lacuesta.net>
2021-10-03 19:34:20 +0100

    Feature: se corriguieron todos los errores previos, ya esta la APP con total funcionamiento

--- src/com/juanSoft/Manage/ManageZoo.java
+++ src/com/juanSoft/Manage/ManageZoo.java
@@ -2,13 +2,15 @@ package com.juanSoft.Manage;
 import com.juanSoft.Animals.Birds;
 import com.juanSoft.Animals.Mammals;
 import com.juanSoft.Animals.Reptiles;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ManageZoo {
         private int amountSpace;
         ArrayList<String>cages = new ArrayList<String>();
+        ArrayList<String> Mammals = new ArrayList<>();
+        ArrayList<String> Birds = new ArrayList<>();
+        ArrayList<String> Reptiles = new ArrayList<>();
         private int nameCage ;
         private int indexCage = -1;
         private int numAnimals;
@@ -16,7 +18,6 @@ public class ManageZoo {
     public int getNumAnimals() {
         return numAnimals;
     }
-
 
         public int getAmountSpace() {
         return amountSpace;
@@ -29,55 +30,58 @@ public class ManageZoo {
         System.out.println("cuantos animales quieres en el zoologico virtual?");
         numAnimals = sc.nextInt();
 
-        createAnimals();
+            try {
+                createAnimals();
+                showDataZoo();
+            }catch(IndexOutOfBoundsException e ) {
+                System.out.println("error: las caracteristicas admitidas son icorrecta");
+            }
 
-        showData();
         sc.close();
     }
 
     public void createAnimals() {
         String name;
         String gender;
-        ArrayList<String> males = new ArrayList<>();
         ArrayList<String> females = new ArrayList<>();
         for (int i = 0; i < getNumAnimals(); i++) {
             int random = (int) (Math.random() * 3) + 1;
             if (random == 1) {
-                name = randomName();
-                gender = randomGender();
-                Mammals mammal = new Mammals(name, 10, gender, 25.5, "diurnos", 50, "Carnivoro");
+                    name = randomName();
+                    gender = randomGender();
+                    Mammals mammal = new Mammals(name, 10, gender, 25.5, "diurnos", 50, "Carnivoro");
                 if (gender == "Male") {
-                    males.add(name + " Mammal " + gender);
+                    Mammals.add(name + " Mammal " + gender);
                 } else {
                     females.add(name + " Mammal " + gender);
                 }
-            } else if (random == 2) {
-                name = randomName();
-                gender = randomGender();
-                Birds bird = new Birds(name, 2, gender, 50.5, "azul", "garra");
+            }else if (random == 2) {
+                    name = randomName();
+                    gender = randomGender();
+                    Birds bird = new Birds(name, 2, gender, 50.5, "azul", "garra");
                 if (gender == "Male") {
-                    males.add(name + " Bird " + gender);
+                    Birds.add(name + " Bird " + gender);
                 } else {
                     females.add(name + " Bird " + gender);
                 }
             } else {
-                name = randomName();
-                gender = randomGender();
-                Reptiles reptile = new Reptiles(randomName(), 1, randomGender(), 25, "verde", 35);
+                    name = randomName();
+                    gender = randomGender();
+                    Reptiles reptile = new Reptiles(randomName(), 1, randomGender(), 25, "verde", 35);
                 if (gender == "Male") {
-                    males.add(name + " Reptile " + gender);
+                    Reptiles.add(name + " Reptile " + gender);
                 } else {
                     females.add(name + " Reptile " + gender);
                 }
 
             }
         }
-        organizeAnimals(males,females);
+        organizeAnimals(females);
     }
 
     public String randomName() {
-        String[] animalsName = {"chiquito", "Mango", "Tifón", "Chimuelo", "Yogu", "Carajito", "Yasuo", "Brego", "Mikasa", "Misifú", "Bley", "Hammer", "Jojo", "Manitas", "Wachin che",
-                "MaxiBon", "Bodoque", "Itachi", "Mariko", "Estrella", "El bicho", "Tarradellas"};
+        String[] animalsName = {"chiquito", "Mango", "Tifón", "Chimuelo", "Yogu", "Carajito", "Yasuo", "Brego", "Mikasa", "Misifú",
+                "Bley", "Hammer", "Jojo", "Manitas", "Wachin che", "MaxiBon", "Bodoque", "Itachi", "Mariko", "Estrella", "El bicho", "Tarradellas"};
         int random = (int) (Math.random() * animalsName.length);
         String name = animalsName[random];
         return name;
@@ -90,66 +94,52 @@ public class ManageZoo {
         return selectGender;
     }
 
-
-    public void organizeAnimals(ArrayList<String> males,ArrayList<String> females) {
-        ArrayList<String> Mammals = new ArrayList<String>();
-        ArrayList<String> Birds = new ArrayList<String>();
-        ArrayList<String> Reptiles = new ArrayList<String>();
-
-
-        for (int i = 0; i <males.size() ; i++) {
-            if (males.get(i).contains("Mammal")) {
-                Mammals.add(males.get(i));
-            } else if (males.get(i).contains("Bird")) {
-                Birds.add(males.get(i));
-            } else {
-                Reptiles.add(males.get(i));
-            }
-        }
+    public void organizeAnimals(ArrayList<String> females) {
 
             int countMamals = -1;
             int countRaptiles = -1;
             int countBirds = -1;
         for (int i = 0; i < females.size() ; i++) {
-             if(females.get(i).contains("Mammal")) {
-                    if(Mammals.isEmpty()) {
+
+            if(females.get(i).contains("Mammal")) {
+                  if(Mammals.isEmpty()) {
                         Mammals.add(females.get(i));
-                        break;
-                    }
-                     countMamals++;
-                   if(Mammals.size() > countMamals) {
-                       Mammals.set(countMamals, Mammals.get(countMamals) + " pareja con " + females.get(i));
-                      } else {
-                             Mammals.add(females.get(i));
-                         }
+                        continue;
+                     }
+                    countMamals++;
+                  if(Mammals.size() > countMamals) {
+                    Mammals.set(countMamals, Mammals.get(countMamals) + " pareja con " + females.get(i));
+                  }else {
+                     Mammals.add(females.get(i));
+                  }
 
-             } else if (females.get(i).contains("Bird")) {
-                 if(Birds.isEmpty()) {
+            } else if (females.get(i).contains("Bird")) {
+                if(Birds.isEmpty()) {
                     Birds.add(females.get(i));
-                    break;
+                    continue;
                 }
-                 countBirds++;
+                countBirds++;
 
-                 if(Birds.size() > countBirds) {
-                        Birds.set(countBirds, Birds.get(countBirds) + " pareja con " + females.get(i));
-                 }else {
-                        Birds.add(females.get(i));
-                    }
+                if(Birds.size() > countBirds) {
+                    Birds.set(countBirds, Birds.get(countBirds) + " pareja con " + females.get(i));
+                }else {
+                    Birds.add(females.get(i));
+                }
 
-             }  else if (females.get(i).contains("Reptile")) {
+            }  else if (females.get(i).contains("Reptile")) {
                 if(Reptiles.isEmpty()) {
                     Reptiles.add(females.get(i));
-                    break;
+                    continue;
                 }
                 countRaptiles++;
-                 if(Reptiles.size() > countRaptiles) {
-                     Reptiles.set(countRaptiles, Reptiles.get(countRaptiles) + " pareja con " + females.get(i));
-                 }else {
-                     Reptiles.add(females.get(i));
-                 }
+                if(Reptiles.size() > countRaptiles) {
+                    Reptiles.set(countRaptiles, Reptiles.get(countRaptiles) + " pareja con " + females.get(i));
+                }else {
+                    Reptiles.add(females.get(i));
+                }
             }
         }
-          createCage();
+        createCage();
 
         for (String s: Mammals) {
             addAnimal(s);
@@ -171,15 +161,16 @@ public class ManageZoo {
         }
     }
 
-    public void showData() {
+    public void showDataZoo() {
         for (String s:cages) {
             System.out.println(s);
         }
     }
 
     public void addAnimal(String animal) {
-         nameCage++;
-         indexCage++;
-        cages.set(indexCage,"cage " +  nameCage + ":" + animal);
+
+            nameCage++;
+            indexCage++;
+            cages.set(indexCage,"cage " +  nameCage + ":" + animal);
     }
 }
